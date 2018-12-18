b9b43f23ce9d7240013378b2c73584301f8a18b9
Bindhu <biji_100@yahoo.com>
2018-12-18 13:07:47 -0700

    changes update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,5 +5,22 @@ var guessesLeft = 9;
 
 //  whenever the user presses a key
 document.onkeyup = function(){
-    
+    // // Randomly chooses a choice from the options array. Computer guess.
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+     
+    // // Determines which key was pressed
+    var userGuess = event.key;
+    var yourGuess = userGuess;
+
+    // // Randomly chooses a choice from the options array. user guess.
+    if (userGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)]){
+        if (userGuess === computerGuess){
+            wins++;  
+        }
+
+        else if (userGuess)
+    }
+
+
+
 }
