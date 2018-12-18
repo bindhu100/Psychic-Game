ab782963b4690aa3bdcfd2ad741706dd51827d22
Bindhu <biji_100@yahoo.com>
2018-12-18 01:40:08 -0700

    changes update

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+var computerChoices = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N","O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+
+//  whenever the user presses a key
+document.onkeyup = function(){
+    
+}
