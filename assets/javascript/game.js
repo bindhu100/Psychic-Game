4114b9b88f761690573100746e829e691db8fadf
Bindhu <biji_100@yahoo.com>
2019-01-09 23:25:25 -0700

    update changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,59 @@
-var computerChoices = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N","O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var computerAlphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N","O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+// variables for tracking the game result
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
+var guessSoFar = [];
+// var letterGuessComp = null;
 
-//  whenever the user presses a key
-document.onkeyup = function(){
-    // // Randomly chooses a choice from the options array. Computer guess.
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-     
-    // // Determines which key was pressed
-    var userGuess = event.key;
-    var yourGuess = userGuess;
+ // computer pick random letters from computerAlphabet arry
+ var coputerGuess = computerAlphabet [Math.floor(Math.random() *computerAlphabet.length)];
 
-    // // Randomly chooses a choice from the options array. user guess.
-    if (userGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)]){
-        if (userGuess === computerGuess){
-            wins++;  
-        }
+// Guesses Left
+var countGuessesLeft = function (){
+    document.querySelector("#guessesLeft").innerHTML="Guesses Left: " + guessesLeft;
+}
 
-        else if (userGuess)
-    }
+// Guesses so far
 
+var userGuessSoFar = function(){
+    document.querySelector("#guesses").innerHTML = "Guesses so far:" + guessSoFar.join(' ');
+}
 
+// countGuessesLeft();
+
+// To restart the game
+
+var restart = function(){
+    guessesLeft = 9;
+    guessSoFar = [];
+    coputerGuess = computerAlphabet [Math.floor(Math.random() *computerAlphabet.length)]; 
 
 }
+
+// when user enter a key
+document.onkeyup = function(event){
+    guessesLeft--; 
+
+    var userGuess = event.key;
+    guessSoFar.push(userGuess);
+    countGuessesLeft();
+    userGuessSoFar();
+
+    // wins
+    if (userGuess === coputerGuess){
+        wins++;
+        document.querySelector("#wins").innerHTML = "Wins:" + wins;
+        restart();
+    }
+
+    // losses
+    else if(guessesLeft === 0){
+        losses++;
+
+        document.querySelector("#losses").innerHTML = "Losses:" + losses;
+        restart();
+    }
+};
+
